78e86ba05307256181143afa82538f6f4adbd538
Saif Siddiqui <saifullah.siddiqui04@gmail.com>
2025-07-31 15:23:15 -0400

    pending rss

---
+++ rss.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"net/http"
+)
+
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	request :=  http.NewRequestWithContext
+	
+
+
+
+
+
+
+	return &RSSFeed{}, nil
+}
